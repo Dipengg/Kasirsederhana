02dd7c6febf59cebf2c519efc2dc1bfa3020e7cc
Dipengg <louisdiaz2003@gmail.com>
2024-03-21 14:15:14 +0700

    update

--- kasir.js
+++ kasir.js
@@ -1,8 +1,8 @@
 // Data Produk (Kode Produk, Nama Produk, Harga) menggunakan array
 let products = [
-  { code: "001", name: "Kaos", price: 50000 },
-  { code: "002", name: "Celana", price: 75000 },
-  { code: "003", name: "Topi", price: 25000 },
+  { code: "P001", name: "Kaos", price: 50000 },
+  { code: "P002", name: "Celana", price: 75000 },
+  { code: "P003", name: "Topi", price: 25000 },
   // tambahkan produk lain di sini
 ];
 
